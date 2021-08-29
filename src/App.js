4b9537e8eb65f1a1a8041c7d85fa4957cd7e485d
Kyle Devine <devinenoise@gmail.com>
2021-08-29 14:39:05 -0700

    using tasks in state hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './index.css';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Take out the trash',
+      day: 'Feb 7th at 1:30pm',
+      reminder: false
+    },
+    {
+      id: 3,
+      text: 'Meeting at the school',
+      day: 'Feb 6th at 10:30am',
+      reminder: true
+    },
+    {
+      id: 4,
+      text: 'Food shopping',
+      day: 'Feb 6th at 2:30pm',
+      reminder: true
+    }
+  ]);
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 };
